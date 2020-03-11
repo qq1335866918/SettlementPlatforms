85c010cfcf006b106319badd07917d95d29f00e3
Your Name <1335866918@qq.com>
2020-03-11 15:48:07 +0800

    2.3

--- app/src/main/java/com/tt1000/settlementplatform/utils/MyUtil.java
+++ app/src/main/java/com/tt1000/settlementplatform/utils/MyUtil.java
@@ -162,6 +162,23 @@ public class MyUtil {
         return s;
     }
 
+    public static String str2HexStr(String str) {
+
+        char[] chars = "0123456789ABCDEF".toCharArray();
+        StringBuilder sb = new StringBuilder("");
+        byte[] bs = str.getBytes();
+        int bit;
+
+        for (int i = 0; i < bs.length; i++) {
+            bit = (bs[i] & 0x0f0) >> 4;
+            sb.append(chars[bit]);
+            bit = bs[i] & 0x0f;
+            sb.append(chars[bit]);
+            sb.append(' ');
+        }
+        return sb.toString().trim();
+    }
+
     public static String getPrimaryValue() {
         try {
             JSONObject random = new JSONObject();
@@ -755,7 +772,7 @@ public class MyUtil {
                         }
                     }
                 } catch (SocketException e) {
-                    Log.e(TAG,"获取3G/4G网络IP失败");
+                    Log.e(TAG, "获取3G/4G网络IP失败");
                 }
             } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {     // wifi
                 WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
@@ -785,6 +802,7 @@ public class MyUtil {
 
     /**
      * 将ip的整数形式转换成ip形式
+     *
      * @param ip
      * @return
      */
